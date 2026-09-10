fd4c12e97ed3f6d1fffc93ac7676bc2926141fde
HERB3LLE <vzn9mg4znh@privaterelay.appleid.com>
2026-09-10 13:08:10 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ let items = [
 {
         type: "photo",
         date: "2025-07-05",
-        title: "띠가 받은 특별한 선물🎁 벨디와의 셀카"
+        title: "띠가 받은 특별한 선물🎁 벨디와의 셀카",
         file: "68F6127C-1305-415F-BE1F-C0A8B710C876.jpeg"
     },
 
