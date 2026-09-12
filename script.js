209c1243e641b8a0d088c9aeaec9511afc457e1b
HERB3LLE <vzn9mg4znh@privaterelay.appleid.com>
2026-09-13 05:03:47 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ let items = [
         date: "2023-08-13",
         title: "솔로곡 체인지🎤",
         file: "솔로곡 체인지🎤#KISSOFLIFE #키스오브라이프 #KIOF #NATTY #나띠 #BELLE #벨#Countdown #카운트다운 #Countdown_Challenge.mp4"
-    }
+    },
 
 ];
 
