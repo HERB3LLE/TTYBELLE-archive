bcad63b8f59c80d875e57b19062f3590f2fd9933
HERB3LLE <vzn9mg4znh@privaterelay.appleid.com>
2026-09-13 22:37:41 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -285,6 +285,48 @@ let items = [
         file: "IMG_8875.webp"
     },
 
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_8952.jpeg"
+    },
+
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_8937.jpeg"
+    },
+
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_8960.jpeg"
+    },
+
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_8948.jpeg"
+    },
+
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_8953.jpeg"
+    },
+
+    {
+        type: "photo",
+        date: "2023-07-05",
+        title: "photocard",
+        file: "IMG_894862.jpeg"
+    },
+
     // =================================
     // 릴스
     // =================================
